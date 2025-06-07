cb657f9c854011c05b213203b7d5e0a187548dd4
Yokoji0129 <132248053+Yokoji0129@users.noreply.github.com>
2025-06-08 03:23:12 +0900

    no message

--- src/components/NavList.vue
+++ src/components/NavList.vue
@@ -1,41 +1,60 @@
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+const navList = ref([
+  { label: "各種申請", path: "/application" },
+  { label: "出勤簿", path: "/work" },
+  { label: "申請履歴", path: "/history" },
+]);
+
+const route = useRoute();
+const isOpen = ref(false);
+</script>
+
 <template>
   <div class="flex h-screen text-base">
-    <!--左側のナビ-->
+    <div class="fixed w-full h-18 z-50 bg-green-500 md:hidden">
+      <button
+        class="absolute top-3 left-3 bg-green-500 text-white py-2.5 px-4 rounded text-2xl"
+        @click="isOpen = !isOpen"
+      >
+        ☰
+      </button>
+    </div>
+
     <nav
-      class="w-64 bg-green-500 text-white flex flex-col justify-between py-4"
+      :class="[
+        'fixed h-full w-full bg-green-500 text-white flex flex-col justify-between pt-18 pb-4 md:py-4 md:w-64 duration-300',
+        isOpen ? 'translate-x-0' : '-translate-x-full',
+        'md:translate-x-0', // デスクトップでは常に表示
+      ]"
     >
       <!--上部メニュー-->
       <div>
-        <ul class="space-y-2 px-2">
-          <li>
+        <ul class="px-2">
+          <li
+            class="md:text-lg lg:text-xl font-bold"
+            v-for="(item, index) in navList"
+            :key="index"
+          >
             <router-link
-              to="/application"
-              class="block hover:bg-green-600 rounded px-3 py-2"
+              :to="item.path"
+              :class="[
+                'block rounded px-3 py-4',
+                route.path === item.path
+                  ? 'bg-green-700'
+                  : 'hover:bg-green-600',
+              ]"
             >
-              各種申請
-            </router-link>
-          </li>
-          <li>
-            <router-link
-              to="/work"
-              class="block hover:bg-green-600 rounded px-3 py-2"
-            >
-              出勤簿
-            </router-link>
-          </li>
-          <li>
-            <router-link
-              to="/history"
-              class="block hover:bg-green-600 rounded px-3 py-2"
-            >
-              申請履歴
+              {{ item.label }}
             </router-link>
           </li>
         </ul>
       </div>
 
       <!--下部メニュー-->
-      <div class="space-y-2 px-2">
+      <div class="space-y-2 px-2 md:text-lg lg:text-xl font-bold">
         <router-link
           to="/support"
           class="block bg-blue-500 hover:bg-blue-600 rounded px-3 py-2"
--- src/views/SupportView.vue
+++ src/views/SupportView.vue
@@ -5,7 +5,7 @@ import NavList from "../components/NavList.vue";
 <template>
   <div class="flex h-screen">
     <NavList />
-    <main class="flex-1 p-6 bg-gray-100 overflow-auto">
+    <main class="flex-1 p-6 bg-gray-100 overflow-auto pt-22 md:ml-64 md:pt-7">
       <h1>サポート</h1>
     </main>
   </div>
